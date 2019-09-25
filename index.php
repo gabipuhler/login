e2b060cfdfc1da57d6778bb918329f15cd53f4dc
gabipuhler <gabipuhler@gmail.com>
2019-09-24 21:27:38 -0300

    Formulário de Recuperação de Senha
    close #2

--- index.php
+++ index.php
@@ -70,6 +70,47 @@ Abra a URL http://localhost/login/ -->
             </form>
             </div>
         </section>
+        <!-- Final da Seção de Login -->
+
+        <!-- Formulário de Recuperação de Senha -->
+        <section class="row mt-5">
+            <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaLogin">
+                <h2 class="text-center mt-2">
+                    Gerar Nova Senha
+                </h2>
+                <form action="#" method="post" id="formSenha" class="p-2">
+                    <div class="form-group">
+                        <small class="text-muted">
+                            Para gerar uma nova senha, digete o
+                            seu e-mail. Clique no link gerado.
+                        </small>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="email" name="emailGerarSenha" 
+                        id="emailGerarSenha" class="form-control"
+                        placeholder="E-mail de recuperação de senha">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="submit" value="::Gerar::"
+                        name="btnGerar" id="btnGerar" 
+                        class="btn btn-primary btn-block">
+                    </div>
+
+                    <div class="form-group">
+                        <p class="text-center">
+                            Já registrado? <a href="#" id="btnJaRegistrado">
+                                Entrar por aqui.
+                            </a>
+                        </p>
+
+                    </div>
+                </form>
+
+            </div>
+        </section>
+        <!-- Fim da Seção de Recuperação de Senha -->
     </main>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
